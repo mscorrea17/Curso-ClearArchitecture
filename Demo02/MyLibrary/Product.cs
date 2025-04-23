200b6d669e14d0a289e89f469e01443b9b27666f
Michael Correa <trabajosmichaelcorrea@gmail.com>
2025-04-23 14:05:22 -0500

    Propiedades y constructores

--- Demo02/MyLibrary/Product.cs
+++ Demo02/MyLibrary/Product.cs
@@ -1,1 +1,25 @@
-﻿
+﻿namespace MyLibrary;
+
+public class Product
+{
+    //  Propiedades
+
+    public int Id { get; }
+
+    public string Name { get; };
+
+    public decimal UnitPrice { get;  };
+
+    public int UnitsInStock { get;  };
+
+    //  Constructor
+
+    public Product(int id, string name, decimal unitPrice, int unitsInStock)
+    {
+        Id = id;
+        Name = name;
+        UnitPrice = unitPrice;
+        UnitsInStock = unitsInStock;
+    }
+}
+
