c55727476622f32c276f758a733ff18fe4b4aead
Michael Correa <trabajosmichaelcorrea@gmail.com>
2025-04-25 10:19:22 -0500

    Encapsulamiento, abstraccion y polimorfismo

---
+++ NorthWind/NorthWind.Entities/ValueObjects/UserAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.ValueObjects;
+    public class UserAction (string user, string description)
+    {
+        public DateTime CreatedDateTime { get; } = DateTime.Now;
+    public string User => user;
+    public string Description => description;
+}
+
