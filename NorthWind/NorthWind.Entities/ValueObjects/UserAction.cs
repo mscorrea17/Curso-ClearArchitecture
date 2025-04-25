eeabcc083c298220a259f68b1797b8e850615965
Michael Correa <trabajosmichaelcorrea@gmail.com>
2025-04-25 10:21:56 -0500

    Fin Sesion 3 - Primera parte

--- NorthWind/NorthWind.Entities/ValueObjects/UserAction.cs
+++ NorthWind/NorthWind.Entities/ValueObjects/UserAction.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NorthWind.Entities.ValueObjects;
-    public class UserAction (string user, string description)
-    {
-        public DateTime CreatedDateTime { get; } = DateTime.Now;
+public class UserAction(string user, string description)
+{
+    public DateTime CreatedDateTime { get; } = DateTime.Now;
     public string User => user;
     public string Description => description;
 }
